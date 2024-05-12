e9ab700804f079661cf4e396509c8bb8ed95b217
takeshisakuma <saude0518@gmail.com>
2024-05-12 23:44:10 +0900

    mod2

--- src/components/input/TextInpute.test.tsx
+++ src/components/input/TextInpute.test.tsx
@@ -2,7 +2,7 @@ import { render, screen } from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 import TextInput from './TextInput'
 
-test('TextInput Component test', async () => {
+test('TextInput Component test mod', async () => {
     const user = userEvent.setup()
     render(<TextInput />)
 
