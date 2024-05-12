f22e6715e97024e29edf43ded5456af033baaa5c
takeshisakuma <saude0518@gmail.com>
2024-05-12 23:34:20 +0900

    mod husky

---
+++ src/components/input/TextInpute.test.tsx
@@ -0,0 +1,16 @@
+import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
+import TextInput from './TextInput'
+
+test('TextInput Component test', async () => {
+    const user = userEvent.setup()
+    render(<TextInput />)
+
+    //input要素が存在することを確認
+    const inputElement = screen.getByRole('textbox')
+    expect(inputElement).toBeInTheDocument()
+
+    //ユーザーがinput要素にHello Worldをタイプして、表示されるのを確認
+    await user.type(inputElement, 'Hello World')
+    expect(screen.getByText('Hello World'))
+})
